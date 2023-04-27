c72bb57019d59ae14664f962f6d6ab7c43c627bd
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-04-27 15:53:14 -0300

    Add files via upload
    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { MoviesContainer } from './components/MoviesContainer';
+
+function App() {
+  return (
+    <>
+      <MoviesContainer />
+    </>
+  );
+}
+
+export default App;
