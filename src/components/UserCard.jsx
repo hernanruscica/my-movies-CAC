dcc9c0c72dd4df26c119fd068c2a5398e36f79c0
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-05-08 15:50:39 -0300

    Add files via upload
    starting with firebase

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,12 +1,36 @@
-export const UserCard = (props) => {       
-    
-    const user = props.user;
-    console.log(user.userName);
 
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore"; //doc returns a single document
+import {db} from "../firebaseConfig/firebase.js";
+import Swal from "sweetalert2"
+import withReactContent from "sweetalert2-react-content"
+const mySwal = withReactContent (Swal)
+
+export const UserCard = (props) => {   
+    
+    //configurar los hooks
+    const [users, setUsers] = useState([])    
+
+    //referenciamos a la db de firestore
+    const usersCollection = collection(db, "users");
+
+    //funcion para mostrar un documento
+
+    //funcion para elimnar un documento
+
+    //funcion para modal de confirmacion de eliminacion del documento
+
+    //useEffect
+    
+    const userTest = props.user;
+    console.log(userTest.userName);
+
+    //devolvemos la vista 
     return (
         <>
         <h1>showing user Card</h1>
-        <p>{`name: ${user.userName}`}</p>        
+        <p>{`name: ${userTest.userName}`}</p>        
         </>
     )
 }
