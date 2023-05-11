4c2358b4b36022f5052c88d5debc5e53f0f9b3e9
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-05-10 21:30:38 -0300

    Add files via upload
    firebase connection is ok!
    But, it needs to be refatored in order to modular it more.

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,36 +1,83 @@
 
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { collection, getDocs, deleteDoc, doc } from "firebase/firestore"; //doc returns a single document
+import { collection, getDocs, getDoc, deleteDoc, doc } from "firebase/firestore"; //doc returns a single document
 import {db} from "../firebaseConfig/firebase.js";
 import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 const mySwal = withReactContent (Swal)
 
-export const UserCard = (props) => {   
-    
-    //configurar los hooks
-    const [users, setUsers] = useState([])    
+export const UserCard =  () => {             
+    //console.log('userCard')
+    const [users, setUsers] = useState([]);
 
-    //referenciamos a la db de firestore
-    const usersCollection = collection(db, "users");
+    const getUserData = async () => {
+        //referenciamos a la db de firestore
+        const usersCollectionRef = collection(db, "users");
+        const userDocRef = doc(usersCollectionRef, "K1jZQ8d1L3LMSmsk24tG");  
+        try {
+            const docSnapshot = await getDoc(userDocRef);
+            if (docSnapshot.exists()) {
+            const userData = docSnapshot.data();
+            //console.log(userData);
+            } else {
+            console.log("El documento no existe");
+            }
+        } catch (error) {
+            console.error("Error al obtener el documento:", error);
+        }
+    };
 
+    /*
+    const getAllUserData = async () => {
+        const usersCollectionRef = collection(db, "users");
+        try {
+          const querySnapshot = await getDocs(usersCollectionRef);          
+          querySnapshot.forEach((doc) => {
+            const userData = doc.data();                       
+            setUsers((user) => [...user, userData]);
+          });                   
+        //console.log(users)
+        } catch (error) {
+          console.error("Error al obtener los documentos:", error);
+        }
+      };
+      */
+      const getAllUserData = async()=>{
+        const usersCollectionRef = collection(db, "users");
+        const data = await getDocs(usersCollectionRef)
+        setUsers(
+            data.docs.map((doc)=>(({...doc.data(),id:doc.id})))
+            )
+            //console.log(users)
+        }
+        
+      
+     useEffect(() => {            
+            getAllUserData();                  
+        }, []); 
+/*
+    useEffect(() => {            
+        console.log(users);                  
+    }, [users]); 
+    */
     //funcion para mostrar un documento
 
     //funcion para elimnar un documento
 
-    //funcion para modal de confirmacion de eliminacion del documento
-
-    //useEffect
+    //funcion para modal de confirmacion de eliminacion del documento       
     
-    const userTest = props.user;
-    console.log(userTest.userName);
 
     //devolvemos la vista 
     return (
         <>
         <h1>showing user Card</h1>
-        <p>{`name: ${userTest.userName}`}</p>        
+        
+        <p>{
+            (users.length !== 0) ? `name: ${users[0]['user_name']}` : ''
+            //users[0]['user_name']
+        }</p>        
+        
         </>
     )
 }
--- src/pages/UserMain.jsx
+++ src/pages/UserMain.jsx
@@ -2,17 +2,12 @@ import "./generalStyles.css";
 import {UserCard} from "../components/UserCard";
 export const UserMain = () => {
 
-    //This should came from the firestore db
-    const user = {
-        userName: 'pepe', 
-        id: 1, 
-        email: 'pepe@yahoo.com', 
-        password: '1234'};
+      
     return (
         <>
             <main className="container">
                 <h1>UserMain Component</h1>
-                <UserCard user={user}></UserCard>
+                <UserCard ></UserCard>
             </main>            
         </>
     )
