9ea6337f9d7a365b1f8023b136100fb0fabfb8ea
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2023-07-25 15:53:16 -0300

    Add files via upload
    cambiado el estilo para los resultados de las busquedas

--- src/components/MoviesSearchResults.jsx
+++ src/components/MoviesSearchResults.jsx
@@ -2,6 +2,7 @@
 import {useState,useEffect} from "react";
 import { GetInfo } from "../utils/MoviesInfo";
 import {Link} from 'react-router-dom';
+import "./MoviesSearchResults.css"
 
 export const MoviesSearchResults = (props) => {       
     
@@ -14,61 +15,47 @@ export const MoviesSearchResults = (props) => {
     const [searchResults, setSearchResults] = useState([]);
     
     useEffect(() => {        
-        GetInfo(`/search/movie?query=${searchText}&year=${searchTextAnio}`).then((data)=>{
-            
-            //console.log(data.results)            
-            //console.log(data.results.filter(result => result.genre_ids.includes(parseInt(searchTextgenero, 10))));
+        GetInfo(`/search/movie?query=${searchText}&year=${searchTextAnio}`).then((data)=>{            
             if(searchTextgenero === '')
                 setSearchResults(data.results);  
             else{
                 setSearchResults(data.results.filter(result => result.genre_ids.includes(parseInt(searchTextgenero, 10))))
             }
         })
-    }, [searchText, searchTextAnio, searchTextgenero]);
-   
-    //searchResults.map((result) => console.log(result.title));
-    
+    }, [searchText, searchTextAnio, searchTextgenero]);   
+    console.log(searchResults ? searchResults : '');
+       
     return (
         <>
-  <main className="container">
-    <h3>
-      Resultados de la búsqueda <strong>"{searchText}"</strong>:
-    </h3>
+            <main className="container">
+                <h3>
+                Resultados de la búsqueda <strong>"{searchText}"</strong>:
+                </h3>                               
 
-    {searchResults.length === 0 ? (
-      <p>No se encontraron resultados.</p>
-        ) : (
-        <table className="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Poster</th>
-                <th scope="col">Título</th>
-                <th scope="col">Año</th>
-                <th scope="col">Enlace</th>
-            </tr>
-            </thead>
-            <tbody>
-            {searchResults.map((movie, index) => (
-                <tr key={index}>
-                <th scope="row">{index}</th>
-                <td>
-                    <img
-                    src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                    alt={`${movie.title} movie poster`}
-                    />
-                </td>
-                <td>{movie.title}</td>
-                <td>{movie.release_date}</td>
-                <td>
-                    <Link to={`/movie/${movie.id}`}>Ver más</Link>
-                </td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-        )}
-    </main>
+                {searchResults.length === 0 ? (
+                <p>No se encontraron resultados.</p>
+                    ) : (
+                     <>
+                     <p>Se muestran los resultados</p>
+
+                     {
+                     searchResults.map((searchResult, index) => (
+                        <div className="movie-result">
+                            <div className="movie-result_img-container"> 
+                                <img src={`https://image.tmdb.org/t/p/w300${searchResult.poster_path}`} 
+                                alt={`"${searchResult.title}" poster`} 
+                                className="movie-result_img-container_img" />
+                            </div>
+                            <div className="movie-result_img-info">
+                                <h2>{searchResult.title} <em>{`(${searchResult.release_date.slice(0, 4)})`} </em></h2>
+                                <p>{searchResult.overview}</p>
+                                <Link to={`/movie/${searchResult.id}`}>Ver más</Link>
+                            </div>
+                        </div>  
+                     ) )}                    
+                    </>  
+                    )}
+            </main>
     </>
 
     )
